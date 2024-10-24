514ba1286850e57467d1f756557f0a0b31d5121d
Neil Darach <neil.darach@gmail.com>
2024-10-24 23:26:43 +0100

    Remove all the references to vim. It will be installed on the system, and will now check for the existance of rust-analyzer in the path before loading rust plugins

--- flake.nix
+++ flake.nix
@@ -2,15 +2,7 @@
   description = "A Devshell for rust development";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixNvim = {
-      #url = "git+file:/home/neil/nixNvim";
-      url = "github:NeilDarach/nixNvim";
-      inputs = {
-        #nixpkgs.follows = "nixpkgs";
-        rust-overlay.follows = "rust-overlay";
-      };
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs = { nixpkgs.follows = "nixpkgs"; };
@@ -20,13 +12,9 @@
   outputs = { self, nixpkgs, flake-utils, nixNvim, rust-overlay, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        inherit (nixNvim) utils;
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (import rust-overlay)
-            (_: prev: { inherit rustToolchain nvim; })
-          ];
+          overlays = [ (import rust-overlay) ];
         };
         rustToolchain = let rust = pkgs.rust-bin;
         in if builtins.pathExists ./rust-toolchain.toml then
@@ -38,16 +26,6 @@
             extensions = [ "rust-src" "rustfmt" ];
           };
 
-        nvim = nixNvim.packages.${system}.default.override (prev: {
-          categoryDefinitions = utils.mergeCatDefs prev.categoryDefinitions
-            ({ pkgs, settings, categories, name, ... }: {
-              lspsAndRuntimeDeps.rust = [ rustToolchain pkgs.rust-analyzer ];
-            });
-          packageDefinitions = prev.packageDefinitions // {
-            nvim = utils.mergeCatDefs prev.packageDefinitions.nvim
-              ({ pkgs, ... }: { categories.rust = true; });
-          };
-        });
       in {
         packages.default =
           let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
@@ -92,10 +70,10 @@
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ nvim rustToolchain just bacon ];
+          buildInputs = with pkgs; [ pkgs.rust-analyzer rustToolchain just bacon ];
           env = {
             RUST_SRC_PATH =
-              "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              "${rustToolchain}/lib/rustlib/src/rust/library";
           };
         };
       });
